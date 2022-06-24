da1ff032f25ae5b539750f7c41c7b2e6b76c14f2
jinglingxing <jinlingbaby@outlook.com>
2022-06-24 11:24:38 -0400

    check why the model is not working

--- src/algorithm/ml.py
+++ src/algorithm/ml.py
@@ -11,28 +11,23 @@ if __name__ == '__main__':
     tf.random.set_seed(42)
     np.random.seed(42)
 
-    X = pd.read_csv('../../data/node_data.csv', sep=',', encoding='latin-1')
-    y = pd.read_csv('../../data/target.csv', sep=',', encoding='latin-1')
+    X = pd.read_csv('../../data/node_data_t.csv', sep=',', encoding='latin-1')
+    y = pd.read_csv('../../data/target_t.csv', sep=',', encoding='latin-1')
     X = X[['time_of_day', 'month_of_year', 'node_number']]
     y = y[['target']]['target']
 
-    sep1, sep2 = int(len(X)*0.7), int(len(X)*0.9)
-    X_train, X_test, X_valid = X[:sep1], X[sep1:sep2], X[sep2:]
-    y_train, y_test, y_valid = y[:sep1] * 10000, y[sep1:sep2]*10000, y[sep2:] * 10000  # change probability to be percentage
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_valid = scaler.transform(X_valid)
-    X_test = scaler.transform(X_test)
+    sep1, sep2, sep3 = int(len(X)*0.05), int(len(X)*0.07), int(len(X)*0.08)
+    X_train, X_valid, X_test = X[:sep1], X[sep1:sep2], X[sep2:sep3]
+    y_train, y_valid, y_test = y[:sep1]*100, y[sep1:sep2]*100, y[sep2:sep3]*100  # change probability to be percentage
 
     model = keras.models.Sequential([
-        keras.layers.Dense(100, activation="sigmoid", kernel_initializer="he_normal", input_shape=X_train.shape[1:]),
-        keras.layers.Dense(30, activation="sigmoid", kernel_initializer="he_normal"),
+        keras.layers.Dense(3, activation="sigmoid", kernel_initializer="he_normal", input_shape=X_train.shape[1:]),
+        keras.layers.Dense(2, activation="sigmoid", kernel_initializer="he_normal"),
         keras.layers.Dense(1)
     ])
 
-    model.compile(loss="mean_squared_logarithmic_error",
-                  optimizer=keras.optimizers.SGD(learning_rate=1e-1),
-                  metrics=['accuracy'])
+    model.compile(loss="mean_squared_error",
+                  optimizer=keras.optimizers.SGD(learning_rate=1e-1))
     history = model.fit(X_train, y_train, epochs=100, validation_data=(X_valid, y_valid),
                         callbacks=[keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)])
 
