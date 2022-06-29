26b183b54c96ca79ad7238ab417abb0fdf9486a2
jinglingxing <jinlingbaby@outlook.com>
2022-06-28 22:10:35 -0400

    added and processed police and fire station data

--- src/features/node_data.py
+++ src/features/node_data.py
@@ -1,6 +1,7 @@
 import random
 import pandas as pd
 import os.path
+from typing import List, Dict
 
 
 def generate_node_data(grid_graph, file_path):
@@ -33,8 +34,44 @@ def generate_node_data(grid_graph, file_path):
         with open(file_path, 'w') as file:
             file.write(node_df.to_csv(line_terminator='\n'))
 
+
+def add_feature(zone_json: Dict, feature: List[Dict]):
+    """
+    add more features to our zone(grid node)
+    :param zone_json: preprocessed_grid_graph
+    :param feature: new feature json file
+    """
+    resolution = zone_json["resolution"]
+    new_feature_dict = {}
+    grid_nodes = zone_json['grid_nodes']
+    for grid_node in grid_nodes:
+        x, y = grid_node['x'], grid_node['y']
+        new_feature_dict[(x, y)] = 0
+
+        for elem in feature:
+            lon, lat = elem['geometry']['coordinates']
+            if abs(grid_node['lat'] - lat) <= resolution/2 and abs(grid_node['lon'] - lon) <= resolution/2:
+                new_feature_dict[(x, y)] += 1
+
+    l = [[x, y, v] for (x, y), v in new_feature_dict.items()]
+    return pd.DataFrame(l, columns=['x', 'y', 'number of police station'])
+
+
 if __name__ == '__main__':
-    from src.models.preprocessing_graph import load_or_process_graph
-    graph = load_or_process_graph('../../data/preprocessed_grid_graph.json')
-    generate_node_data(graph, '../../data/grid_node_data.csv')
+    # from src.models.preprocessing_graph import load_or_process_graph
+    # graph = load_or_process_graph('../../data/preprocessed_grid_graph.json')
+    # generate_node_data(graph, '../../data/grid_node_data.csv')
+    import json
+    grid_graph_json = json.load(open('../../data/preprocessed_grid_graph.json', 'r'))
+    police_json = json.load(open('../../data/police-station.json'))
+    police_features = police_json['features']
+    police_station = add_feature(grid_graph_json, police_features)
+    with open('../../data/police-station.csv', 'w') as file:
+        file.write(police_station.to_csv(line_terminator='\n'))
+    
+    fire_json = json.load(open('../../data/fire-station.json'))
+    fire_features = fire_json['features']
+    fire_station = add_feature(grid_graph_json, fire_features)
+    with open('../../data/fire-station.csv', 'w') as file:
+        file.write(fire_station.to_csv(line_terminator='\n'))
 
